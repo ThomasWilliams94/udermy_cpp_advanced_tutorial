30a2bb36cb6c8aa8799aa165e8a2729fe1bb3669
Thomas Williams <thomas.williams@live.co.uk>
2016-11-06 18:32:26 +0100

    Starting new project. Lecture 003 - Standard Exceptions.

---
+++ workspace/003_StandardExceptions/src/003_StandardExceptions.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : 003_StandardExceptions.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
