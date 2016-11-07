2dba9e9af237eb4a1c0864c966216ff2ae842723
Thomas Williams <thomas.williams@live.co.uk>
2016-11-07 17:58:20 +0100

    Doing the suggested exercise for lecture 006.

--- workspace/006_WritingTextFiles/src/006_WritingTextFiles.cpp
+++ workspace/006_WritingTextFiles/src/006_WritingTextFiles.cpp
@@ -20,12 +20,30 @@ int main() {
 	fstream outFile;
 	outFile.open(outputFileName, ios::out);
 
-	if(outFile.is_open()) {
+	if (outFile.is_open()) {
 		outFile << "Hello there." << endl;
 		outFile << 123 << endl;
 		outFile.close();
 	} else {
 		cout << "Could not create file: " << outputFileName << endl;
+	}
+
+	// Exercise - write the same line to a file x number of times, numbering
+	// the rows
+
+	const char *repeatedTextOutputFileName = "output/repeated_text_output.txt";
+	string repeatedText = "repeat me!";
+
+	ofstream repeatedOutFile;
+	repeatedOutFile.open(repeatedTextOutputFileName);
+
+	if (repeatedOutFile.is_open()) {
+		for (int i = 0; i < 10; i++) {
+			repeatedOutFile << i+1 << ". " << repeatedText << endl;
+		}
+		repeatedOutFile.close();
+	} else {
+		cout << "Could not create file: " << repeatedTextOutputFileName << endl;
 	}
 
 	return 0;
