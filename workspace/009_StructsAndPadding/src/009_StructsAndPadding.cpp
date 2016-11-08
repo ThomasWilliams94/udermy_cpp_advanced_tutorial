8b6ca05bdd6109f3f852d39ad8fafeb3ed609f99
Thomas Williams <thomas.williams@live.co.uk>
2016-11-08 14:22:29 +0100

    Lecture 010 - Reading and Writing Binary Files (Continued from previous lecture).

--- workspace/009_StructsAndPadding/src/009_StructsAndPadding.cpp
+++ workspace/009_StructsAndPadding/src/009_StructsAndPadding.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #pragma pack(push, 1)
@@ -14,13 +15,52 @@ using namespace std;
 struct Person {
 	char name[50];
 	int age;
-	double weight;
+	double height;
 };
 
 #pragma pack(pop)
 
 int main() {
 
-	cout << sizeof(Person) << endl;
+	Person someone = { "Frodo", 220, 0.8 };
+
+	const char *filePath = "output/test.bin";
+
+	/// Write binary file ////////////////////////
+	ofstream outputFile;
+
+	outputFile.open(filePath, ios::binary);
+
+	if (outputFile.is_open()) {
+
+		//outputFile.write((char *)&someone, sizeof(Person)); // Old way of casting point to char pointer
+		outputFile.write(reinterpret_cast<char *>(&someone), sizeof(Person));
+
+		outputFile.close();
+
+	} else {
+		cout << "Could not create file: " << filePath << endl;
+	}
+
+	// Read binary file /////////////
+
+	Person someoneElse = {};
+
+	ifstream inputFile;
+
+	inputFile.open(filePath, ios::binary);
+
+	if (inputFile.is_open()) {
+
+		//inputFile.write((char *)&someone, sizeof(Person)); // Old way of casting point to char pointer
+		inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
+
+		inputFile.close();
+
+	} else {
+		cout << "Could not read file: " << filePath << endl;
+	}
+
+	cout << someoneElse.name << ", " << someoneElse.age << ", " << someoneElse.height << endl;
 	return 0;
 }
