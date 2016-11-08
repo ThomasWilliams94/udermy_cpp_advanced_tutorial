e78e195241ee5dd7266247a76b92bc7e13c57ba4
Thomas Williams <thomas.williams@live.co.uk>
2016-11-08 09:53:55 +0100

    Writing code for lecture 007.

--- workspace/007_ReadingTextFiles/src/007_ReadingTextFiles.cpp
+++ workspace/007_ReadingTextFiles/src/007_ReadingTextFiles.cpp
@@ -7,9 +7,36 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
+
+	const char *inFilePath = "input/test.txt";
+
+	ifstream inFile;
+
+	inFile.open(inFilePath);
+
+	if (inFile.is_open()) {
+		string line;
+
+//		while (!inFile.eof()) {
+//			getline(inFile, line);
+//			cout << line << endl;
+//		}
+
+		// This invokes code to see if inFile still has input
+		// to be read (inFile bool operator is overloaded?)
+		while (inFile) {
+			getline(inFile, line);
+			cout << line << endl;
+		}
+
+		inFile.close();
+	} else {
+		cout << "Cannot open file: " << inFilePath << endl;
+	}
 
 	return 0;
 }
