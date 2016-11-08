c6d099b1b8e3b2b033bb945ea8e618c0aabf5199
Thomas Williams <thomas.williams@live.co.uk>
2016-11-08 13:52:26 +0100

    Writing code for lecture 008.

--- workspace/008_ParsingTextFiles/src/008_ParsingTextFiles.cpp
+++ workspace/008_ParsingTextFiles/src/008_ParsingTextFiles.cpp
@@ -7,9 +7,41 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
+
+	const char *filepath = "input/stats.txt";
+	ifstream input;
+
+	input.open(filepath);
+
+	if(!input.is_open()) {
+		return 1;
+	}
+
+	string line;
+	int population;
+
+	while(input) {
+		getline(input, line, ':');
+
+		int popultion;
+		input >> population;
+
+		//input.get();
+		input >> ws; // C++11, ws = read white space;
+
+		if(!input) {
+			break;
+		}
+
+		cout << "'" << line << "'" << " -- " << "'" << population << "'" << endl;
+	}
+
+	input.close();
+
 
 	return 0;
 }
