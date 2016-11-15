588b34aea8b1fa2eb5a26b594e47f56f17f1c477
Thomas Williams <thomas.williams@live.co.uk>
2016-11-15 12:50:01 +0100

    Completeing optional challenge for lecture 013.

--- workspace/013_TwoDimensionalVectors/src/013_TwoDimensionalVectors.cpp
+++ workspace/013_TwoDimensionalVectors/src/013_TwoDimensionalVectors.cpp
@@ -7,22 +7,46 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 using namespace std;
 
 int main() {
 
-	vector< vector<int> > grid(3, vector<int>(4, 7));
+	vector<vector<int> > grid(3, vector<int>(4, 7));
 
 	grid[1].push_back(8);
 
-	for(int row = 0; row < grid.size(); row++) {
-		for(int col = 0; col < grid[row].size(); col++) {
+	for (int row = 0; row < grid.size(); row++) {
+		for (int col = 0; col < grid[row].size(); col++) {
 			cout << grid[row][col] << " " << flush;
 		}
 		cout << endl;
 	}
 
+	cout << endl;
+
+	// Create and display the 12 times tables
+	vector<vector<int> > timesTable(12, vector<int>(12, 0));
+
+	for (int row = 0; row < timesTable.size(); row++) {
+		for (int col = 0; col < timesTable[row].size(); col++) {
+			timesTable[row][col] = (row + 1) * (col + 1);
+		}
+	}
+
+	for (int row = 0; row < timesTable.size(); row++) {
+		for (int col = 0; col < timesTable[row].size(); col++) {
+
+			// There must be a better way of doing this formatting... :)
+			cout << setw(3);
+			cout.fill(' ');
+			cout << left;
+
+			cout << timesTable[row][col] << " " << flush;
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
