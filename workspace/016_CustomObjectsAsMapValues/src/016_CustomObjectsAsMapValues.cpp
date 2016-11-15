efce082113ae1120ba3189c0d3010671602db5b1
Thomas Williams <thomas.williams@live.co.uk>
2016-11-15 14:18:26 +0100

    Writing code for lecture 016.

--- workspace/016_CustomObjectsAsMapValues/src/016_CustomObjectsAsMapValues.cpp
+++ workspace/016_CustomObjectsAsMapValues/src/016_CustomObjectsAsMapValues.cpp
@@ -7,10 +7,48 @@
 //============================================================================
 
 #include <iostream>
+#include <map>
 using namespace std;
+
+class Person {
+private:
+	string name;
+	int age;
+
+public:
+	// must provide a default constructor if you want to use your
+	// custom object with maps
+	Person() : name(""), age(0) {
+
+	}
+
+	Person(const Person &other) {
+		cout << "Copy constructor running." << endl;
+		name = other.name;
+		age = other.age;
+	}
+
+	Person(string name, int age) :
+			name(name), age(age) {
+	}
+
+	void print() {
+		cout << name << ": " << age << endl;
+	}
+};
 
 int main() {
 
+	map<int, Person> people;
+
+	people[2] = Person("Mike", 40);
+	people[1] = Person("Vicky", 30);
+	people[5] = Person("Raj", 20);
+
+	for(map<int, Person>::iterator it = people.begin(); it != people.end(); it++) {
+		cout << it->first << ": " << flush;
+		it->second.print();
+	}
 
 	return 0;
 }
