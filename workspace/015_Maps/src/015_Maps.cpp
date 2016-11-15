46ddd61d0893c0f399875ade9c74ecc7526db830
Thomas Williams <thomas.williams@live.co.uk>
2016-11-15 13:45:49 +0100

    Writing code for lecture 015.

--- workspace/015_Maps/src/015_Maps.cpp
+++ workspace/015_Maps/src/015_Maps.cpp
@@ -7,9 +7,49 @@
 //============================================================================
 
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main() {
+
+	map<string, int> ages;
+
+	ages["Mike"] = 40;
+	ages["Raj"] = 20;
+	ages["Vicky"] = 30;
+
+	ages["Mike"] = 70;
+
+	pair<string, int> addToMap("Peter", 100);
+	ages.insert(addToMap); // could use pair constructor directly
+
+	ages.insert(make_pair("Thomas", 22)); // handy way of quickly adding a new pair
+
+	cout << ages["Mike"] << endl;
+
+	cout << ages["Sue"] << endl; // This will actually add "Sue" to the map!
+
+	if (ages.find("Vicky") != ages.end()) {
+		cout << "Found Vicky!" << endl;
+	} else {
+		cout << "Key not found." << endl;
+	}
+
+	if (ages.find("Jemima") != ages.end()) {
+		cout << "Found Jemima!" << endl;
+	} else {
+		cout << "Key not found." << endl;
+	}
+
+	for (map<string, int>::iterator it = ages.begin(); it != ages.end(); it++) {
+		cout << it->first << ": " << it->second << endl;
+	}
+
+	for (map<string, int>::iterator it = ages.begin(); it != ages.end(); it++) {
+		pair<string, int> age = *it;
+
+		cout << age.first << ": " << age.second << endl;
+	}
 
 	return 0;
 }
