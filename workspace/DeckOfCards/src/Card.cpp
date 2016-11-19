673300f40cb48ad3fa147d971c90c39eef73c874
Thomas Williams <thomas.williams@live.co.uk>
2016-11-19 11:56:14 +0100

    Adding a new Card class.

---
+++ workspace/DeckOfCards/src/Card.cpp
@@ -0,0 +1,21 @@
+/*
+ * Card.cpp
+ *
+ *  Created on: 19 Nov 2016
+ *      Author: Thomas
+ */
+
+#include "Card.h"
+
+namespace tjw {
+
+Card::Card() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Card::~Card() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace tjw */
---
+++ workspace/DeckOfCards/src/Card.h
@@ -0,0 +1,21 @@
+/*
+ * Card.h
+ *
+ *  Created on: 19 Nov 2016
+ *      Author: Thomas
+ */
+
+#ifndef CARD_H_
+#define CARD_H_
+
+namespace tjw {
+
+class Card {
+public:
+	Card();
+	virtual ~Card();
+};
+
+} /* namespace tjw */
+
+#endif /* CARD_H_ */
