7fdb245b60a0ecee195daa395b32bfcb672ba0fb
Thomas Williams <thomas.williams@live.co.uk>
2016-11-19 17:30:40 +0100

    Writing out code for lecture 026.

--- workspace/026_OverloadingPlus/src/026_OverloadingPlus.cpp
+++ workspace/026_OverloadingPlus/src/026_OverloadingPlus.cpp
@@ -7,9 +7,23 @@
 //============================================================================
 
 #include <iostream>
+#include "Complex.h"
+
 using namespace std;
+using namespace tjw;
 
 int main() {
+
+	Complex c1(3, 4);
+	Complex c2(5, 1);
+
+	cout << c1 << endl;
+	cout << c2 << endl;
+	cout << c1 + c2 << endl;
+
+	cout << c2 + 8.2 << endl;
+
+	cout << 8.2 + c2 << endl;
 
 	return 0;
 }
