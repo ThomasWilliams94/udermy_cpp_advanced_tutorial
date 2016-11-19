1d1a54babbd5222f6e90b980390d4ecad466c27e
Thomas Williams <thomas.williams@live.co.uk>
2016-11-19 17:48:13 +0100

    Writing code for lecture 028.

--- workspace/028_OverloadingDereferenceOperator/src/028_OverloadingDereferenceOperator.cpp
+++ workspace/028_OverloadingDereferenceOperator/src/028_OverloadingDereferenceOperator.cpp
@@ -7,9 +7,17 @@
 //============================================================================
 
 #include <iostream>
+#include "Complex.h"
 using namespace std;
+using namespace tjw;
 
 int main() {
+
+	Complex c1(2, 4);
+
+	cout << c1 << endl;
+
+	cout << *c1 + c1 << endl;
 
 	return 0;
 }
