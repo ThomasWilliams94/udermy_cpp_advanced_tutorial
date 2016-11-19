8aae90a2756da5928206b754a5a3ef539e9bb107
Thomas Williams <thomas.williams@live.co.uk>
2016-11-19 10:21:04 +0100

    Writing out code for lecture 020.

--- workspace/020_StacksAndQueues/src/020_StacksAndQueues.cpp
+++ workspace/020_StacksAndQueues/src/020_StacksAndQueues.cpp
@@ -7,9 +7,90 @@
 //============================================================================
 
 #include <iostream>
+#include <stack>
+#include <queue>
 using namespace std;
 
+class Test {
+private:
+	string name;
+
+public:
+	Test(string name) :
+			name(name) {
+
+	}
+
+	~Test() {
+		//cout << "Object destroyed." << endl;
+	}
+
+	void print() {
+		cout << name << endl;
+	}
+};
+
 int main() {
+
+	////// STACK //////
+	// last in, first out (LIFO)
+	stack<Test> testStack;
+
+	testStack.push(Test("Mike"));
+	testStack.push(Test("John"));
+	testStack.push(Test("Sue"));
+
+	/*
+	 * This is invalid code! Object is destroyed.
+	 Test &test1 = testStack.top();
+	 testStack.pop();
+	 test1.print(); // Reference refers to destroyed object. Remove & to use a copy that will persist
+
+	 */
+
+	// A sort of way to iterate through stacks, but actually involves
+	// removing all objects on the stack, in reverse order.
+	while (testStack.size() > 0) {
+		Test &test = testStack.top();
+		test.print();
+
+		testStack.pop();
+	}
+
+	////// END STACK //////
+
+	cout << endl;
+
+	////// QUEUE //////
+
+	// first in, first out (FIFO)
+	queue<Test> testQueue;
+
+	testQueue.push(Test("Mike"));
+	testQueue.push(Test("John"));
+	testQueue.push(Test("Sue"));
+
+	/*
+	 * This is invalid code! Object is destroyed.
+	 Test &test1 = testStack.top();
+	 testStack.pop();
+	 test1.print(); // Reference refers to destroyed object. Remove & to use a copy that will persist
+
+	 */
+
+	cout << "Back of queue is: " << flush;
+	testQueue.back().print();
+
+	// A sort of way to iterate through stacks, but actually involves
+	// removing all objects on the stack, in reverse order.
+	while (testQueue.size() > 0) {
+		Test &test = testQueue.front();
+		test.print();
+
+		testQueue.pop();
+	}
+
+	////// END QUEUE //////
 
 	return 0;
 }
