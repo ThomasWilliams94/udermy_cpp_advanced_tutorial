5b78f894b96c282055559e3082633cefcda1ca18
Thomas Williams <thomas.williams@live.co.uk>
2016-11-19 10:23:45 +0100

    Starting new project. Lecture 021 - Sorting Vectors, Deque and Friend.

---
+++ workspace/021_SortingVectors_DequeAndFriend/src/021_SortingVectors_DequeAndFriend.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 021_SortingVectors_DequeAndFriend.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
