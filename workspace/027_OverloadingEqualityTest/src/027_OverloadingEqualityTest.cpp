76dd520cb326d9f8aa7a9bbac2ee42841eec2afa
Thomas Williams <thomas.williams@live.co.uk>
2016-11-19 17:43:25 +0100

    Writing out code for lecture 027.

--- workspace/027_OverloadingEqualityTest/src/027_OverloadingEqualityTest.cpp
+++ workspace/027_OverloadingEqualityTest/src/027_OverloadingEqualityTest.cpp
@@ -7,10 +7,26 @@
 //============================================================================
 
 #include <iostream>
+#include "Complex.h"
 using namespace std;
+using namespace tjw;
 
 int main() {
 
+	Complex c1(3, 2);
+	Complex c2(3, 2);
+
+	if (c1 == c2) {
+		cout << "Equal" << endl;
+	} else {
+		cout << "Not equal" << endl;
+	}
+
+	if (c1 != c2) {
+		cout << "Not equal" << endl;
+	} else {
+		cout << "Equal" << endl;
+	}
 
 	return 0;
 }
