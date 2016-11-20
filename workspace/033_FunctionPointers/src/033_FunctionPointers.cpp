bd0a922089213f57bc0dcf7f89058bca8bd2d677
Thomas Williams <thomas.williams@live.co.uk>
2016-11-20 13:48:21 +0100

    Writing out code (and doing exercise) for lecture 033.

--- workspace/033_FunctionPointers/src/033_FunctionPointers.cpp
+++ workspace/033_FunctionPointers/src/033_FunctionPointers.cpp
@@ -7,9 +7,58 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
 using namespace std;
 
+void test() {
+	cout << "Hello" << endl;
+}
+
+void test2(int value) {
+	cout << "Hello: " << value << endl;
+}
+
+string test3(int value) {
+	// Converting the int to a string was much more complicated than I thought!
+	// to_string() doesn't work before C++11.... could rework the below code
+	// into your own "intToString" method
+	stringstream ss;
+
+	ss << "The value given was: ";
+	ss << value;
+
+	return ss.str();
+}
+
 int main() {
+
+	test();
+
+	void (*pTest)(); // void (*pTest)() = test; also works
+
+	//pTest = &test;
+	pTest = test;
+
+	//(*pTest)();
+	pTest();
+
+	cout << endl;
+
+	test2(10);
+
+	void (*pTest2)(int) = test2;
+
+	pTest2(8);
+
+	cout << endl;
+
+	cout << test3(10) << endl;
+
+	string (*pTest3)(int) = test3;
+
+	string myString = pTest3(8);
+
+	cout << myString << endl;
 
 
 	return 0;
