75a59f6c8d0b2a4d817c0ff69409b8515a9bedaa
Thomas Williams <thomas.williams@live.co.uk>
2016-11-20 13:13:56 +0100

    Writing out code for lecture 031.

--- workspace/031_TemplateFunctions/src/031_TemplateFunctions.cpp
+++ workspace/031_TemplateFunctions/src/031_TemplateFunctions.cpp
@@ -9,7 +9,19 @@
 #include <iostream>
 using namespace std;
 
+template<class T>
+void print(T n) {
+	cout << n << endl;
+}
+
 int main() {
+
+
+	print<int>(5);
+	print<string>("Alright?");
+
+
+	print("He there");
 
 	return 0;
 }
