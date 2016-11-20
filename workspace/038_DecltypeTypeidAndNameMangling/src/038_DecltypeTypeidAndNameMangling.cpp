abc6865c8515a1204b4dfda814cd98f4de6e7fe2
Thomas Williams <thomas.williams@live.co.uk>
2016-11-20 16:11:09 +0100

    Writing code for lecture 038.

--- workspace/038_DecltypeTypeidAndNameMangling/src/038_DecltypeTypeidAndNameMangling.cpp
+++ workspace/038_DecltypeTypeidAndNameMangling/src/038_DecltypeTypeidAndNameMangling.cpp
@@ -7,10 +7,21 @@
 //============================================================================
 
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 int main() {
 
+	string value;
+
+	cout << typeid(value).name() << endl;
+
+	// C++11 specific
+	decltype(value) name = "bob";
+
+	cout << typeid(value).name() << endl;
+
+	cout << name << endl;
 
 	return 0;
 }
