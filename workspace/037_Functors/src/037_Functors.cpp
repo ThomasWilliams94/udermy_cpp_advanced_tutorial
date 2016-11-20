447959dfe287d4d11a2b90a7a65041a8f07de854
Thomas Williams <thomas.williams@live.co.uk>
2016-11-20 15:27:27 +0100

    Writing code for lecture 037.

--- workspace/037_Functors/src/037_Functors.cpp
+++ workspace/037_Functors/src/037_Functors.cpp
@@ -9,8 +9,36 @@
 #include <iostream>
 using namespace std;
 
+struct Test {
+	virtual bool operator()(string &text)=0;
+	virtual ~Test(){};
+};
+
+// a functor is a class that overloads () operator
+struct MatchTest : public Test {
+	virtual bool operator()(string &text) {
+		return text == "lion";
+	}
+};
+
+void check(string text, Test &test) {
+	if(test(text)) {
+		cout << "Text matches!" << endl;
+	} else {
+		cout << "No match." << endl;
+	}
+}
+
 int main() {
 
+	MatchTest pred;
+
+	string value = "lidon";
+
+	cout << pred(value) << endl;
+
+	MatchTest m;
+	check("lifon", m);
 
 	return 0;
 }
