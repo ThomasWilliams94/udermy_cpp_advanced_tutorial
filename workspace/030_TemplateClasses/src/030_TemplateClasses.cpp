3bdcc6944123dbc3484320043bf3cd1124410f83
Thomas Williams <thomas.williams@live.co.uk>
2016-11-20 13:08:58 +0100

    Writing out code for lecture 030.

--- workspace/030_TemplateClasses/src/030_TemplateClasses.cpp
+++ workspace/030_TemplateClasses/src/030_TemplateClasses.cpp
@@ -9,7 +9,26 @@
 #include <iostream>
 using namespace std;
 
+template<class T>
+class Test {
+private:
+	T obj;
+
+public:
+	Test(T obj) {
+		this->obj = obj;
+	}
+
+	void print() {
+		cout << obj << endl;
+	}
+};
+
 int main() {
+
+	Test<string> test1("Hello");
+	test1.print();
+
 
 	return 0;
 }
