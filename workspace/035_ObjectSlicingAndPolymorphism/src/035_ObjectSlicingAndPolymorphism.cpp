22b7ec502bcfa95eed67fc4cc9e6f442d13407e4
Thomas Williams <thomas.williams@live.co.uk>
2016-11-20 14:21:47 +0100

    Writing out code for lecture 035.

--- workspace/035_ObjectSlicingAndPolymorphism/src/035_ObjectSlicingAndPolymorphism.cpp
+++ workspace/035_ObjectSlicingAndPolymorphism/src/035_ObjectSlicingAndPolymorphism.cpp
@@ -9,7 +9,53 @@
 #include <iostream>
 using namespace std;
 
+class Parent {
+private:
+	int one;
+
+public:
+	virtual void print() {
+		cout << "parent" << endl;
+	}
+
+	Parent() : one(0) {
+
+	}
+
+	Parent(const Parent &other) : one(0) {
+		one = other.one;
+		cout << "copy parent" << endl;
+	}
+
+	virtual ~Parent() {
+
+	}
+};
+
+class Child: public Parent {
+private:
+	int two;
+
+public:
+	Child() : two(0) {
+
+	}
+
+	void print() {
+		cout << "child" << endl;
+	}
+};
+
 int main() {
+
+	Child c1;
+	Parent &p1 = c1;
+	p1.print();
+
+	cout << endl;
+
+	Parent p2 = Child(); // invoking copy constructor
+	p2.print();
 
 	return 0;
 }
