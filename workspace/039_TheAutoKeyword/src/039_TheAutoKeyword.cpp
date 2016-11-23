27cafdf9a0e39572a0371418394eaaeb4b52efda
Thomas Williams <thomas.williams@live.co.uk>
2016-11-23 10:24:58 +0100

    Writing code for lecture 039.

--- workspace/039_TheAutoKeyword/src/039_TheAutoKeyword.cpp
+++ workspace/039_TheAutoKeyword/src/039_TheAutoKeyword.cpp
@@ -9,8 +9,34 @@
 #include <iostream>
 using namespace std;
 
+template <class T, class S>
+auto test(T value1, S value2) -> decltype(value1 + value2) {
+	return value1 + value2;
+}
+
+int get() {
+	return 999;
+}
+
+auto test2() -> decltype(get()) {
+	return get();
+}
+
 int main() {
 
+	auto value = 7;
+	auto text = "Hello";
+
+	string text2 = "First part of text,";
+
+	cout << value << endl;
+	cout << text << endl;
+
+	cout << test(100, 8) << endl;
+	cout << test(text2, " plus some more text") << endl;
+
+
+	cout << test2() << endl;
 
 	return 0;
 }
