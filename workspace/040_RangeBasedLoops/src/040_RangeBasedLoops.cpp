a5cfc22bada6473a057e07c278d35c8d3df8a909
Thomas Williams <thomas.williams@live.co.uk>
2016-11-23 10:26:38 +0100

    Starting new project. Lecture 040 - Range-based Loops.

---
+++ workspace/040_RangeBasedLoops/src/040_RangeBasedLoops.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 040_RangeBasedLoops.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
